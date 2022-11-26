b35de14d91ddc11e03b867d9755a8744a3b882b2
Самир <s.gahramanov@intensa.ru>
2022-11-26 13:57:35 +0300

    Привел в аорядок шаблон

--- public/index.php
+++ public/index.php
@@ -1,37 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Weather App</title>
+</head>
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Octopus\WeatherApp\Controllers\WeatherAppController;
-
 try {
-    $app = (new WeatherAppController())->getApp();
+    $app = (new Octopus\WeatherApp\Controllers\WeatherAppController())->getApp();
 } catch (\Exception $e) {
     echo 'Sorry, try later';
 }
-
-if (isset($app) && !empty($app)) :?>
+?>
+<body>
+<? if (isset($app) && !empty($app)) : ?>
     <div class="weather">
         <div class="weather__wrapper weather-data">
             <span class="weather-data__weather">
-                Погода в&nbsp;местоположении <?php echo $app->getLocality()?>
+                Погода в  <?php echo $app->getLocality() ?>
             </span>
             <span class="weather-data__location">
-                Температура <?php echo $app->getTemperature()?>&#176;С
+                Температура <?php echo $app->getTemperature() ?>&#176;С
             </span>
             <span class="weather-data__feels">
-                Температура чувствуется как <?php echo $app->getFeels()?>&#176;С
+                Температура чувствуется как <?php echo $app->getFeels() ?>&#176;С
             </span>
             <span class="weather-data__feels">
-                Влажность <?php echo $app->getHumidity()?>%
+                Влажность <?php echo $app->getHumidity() ?>%
             </span>
             <span class="weather-data__feels">
-                Скорость ветра <?php echo $app->getWindSpeed()?> м/с
+                Скорость ветра <?php echo $app->getWindSpeed() ?> м/с
             </span>
         </div>
     </div>
-<?php endif;?>
-
+<?php endif; ?>
+</body>
+</html>
 <style>
     .weather-data span {
         display: block;
